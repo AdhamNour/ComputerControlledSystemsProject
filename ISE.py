01b5323b2b9423c006467432c1ac31402cb43b15
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-06-01 20:10:23 +0200

    fixed bug

--- ISE.py
+++ ISE.py
@@ -6,11 +6,9 @@ def ISE(yapprox,Tsampling):
     summedArea=0
     summedAreaVector=[]
     for i in range(n):
-        for j in range(n):
             error = getPointInAnalyticalModel(i*Tsampling)-yapprox[i]
             error2 = math.pow(error,2)
             summedArea = summedArea + error2*Tsampling
-        summedAreaVector.append(summedArea)
-        summedArea=0
+            summedAreaVector.append(summedArea)
 
     return summedAreaVector
